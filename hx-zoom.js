916031b7a6f00e64fa9b75ae48a5e6b4c02516bc
Colin-Fredericks <colin.fredericks@gmail.com>
2020-11-30 18:57:34 -0500

    Lazy loading

--- hx-zoom.js
+++ hx-zoom.js
@@ -1,9 +1,20 @@
-// Get an array of the images to zoom.
+// Get an array of the divs that will hold images.
 let image_array = [];
-let zoom_images = document.getElementsByClassName('zoom_img');
+let zoom_boxes = document.querySelectorAll('.zoom_box');
+
+// Put blank images inside the divs.
+Object.keys(zoom_boxes).forEach(function (i) {
+  let img_tag = document.createElement('img');
+  img_tag.attributes.src = '';
+  img_tag.attributes.alt = '';
+  img_tag.className = 'zoom_img';
+  zoom_boxes[i].appendChild(img_tag);
+});
+
+let zoom_images = document.querySelectorAll('.zoom_img');
 
 // Get the slider.
-let slider = document.getElementById('zoomrange');
+let slider = document.getElementById('zoom_slider');
 setVisibility(slider.value);
 
 // Update the current slider value (each time you drag the slider handle)
@@ -12,18 +23,16 @@ slider.oninput = function () {
   // console.log(this.value);
 };
 
+// Handle visibility when slider moves.
 function setVisibility(zoom) {
-  let shown = [];
-  Object.keys(zoom_images).forEach(function (i) {
-    let img = zoom_images[i];
-    let d = zoom_images[i].dataset;
-    // console.log(d.firstVis <= zoom && zoom <= d.lastVis);
+  Object.keys(zoom_boxes).forEach(function (i) {
+    let box = zoom_boxes[i];
+    let d = zoom_boxes[i].dataset;
     if (Number(d.firstVis) <= zoom && zoom <= Number(d.lastVis)) {
-      zoom_images[i].style.display = 'block';
-      shown.push(i);
+      zoom_boxes[i].style.display = 'block';
+      zoom_images[i].src = d.src;
     } else {
-      zoom_images[i].style.display = '';
+      zoom_boxes[i].style.display = '';
     }
   });
-  console.log(shown);
 }
